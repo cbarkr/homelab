9a1ef5132dca35a05ade83b0f36e1236d22b21ae
Callum Barker <cbarkr@gmail.com>
2026-01-09 13:40:35 -0800

    update backup script to activate env vars relative to script location

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-source ./.env
+# Activate `.env` relative to script location
+dirname="$(dirname "$0")"
+envpath="$dirname/.env"
+source "$envpath"
 
+# Set vars for backup
 src=${BACKUP_SRC}
 dst=${BACKUP_DST}
 log="/var/log/backup.log"
 
 echo "Backup started at $(date)" >> "$log"
 
+# Perform the backup itself
 rsync -av --delete "$src" "$dst" >> "$log" 2>&1
 
 echo "Backup completed at $(date)" >> "$log"
